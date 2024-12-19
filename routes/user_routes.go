8963b9241cdae53cf3675235b12b72fb90c02495
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-12-19 09:34:32 -0300

    added cors

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -8,9 +8,9 @@ import (
 
 func RegisterUserRoutes(router *mux.Router) {
 	subRouter := router.PathPrefix("/users").Subrouter()
-	subRouter.HandleFunc("", handlers.CreateUser).Methods("POST")
-	subRouter.HandleFunc("", handlers.GetUsers).Methods("GET")
-	subRouter.HandleFunc("/{id:[0-9]+}", handlers.GetUser).Methods("GET")
-	subRouter.HandleFunc("/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
-	subRouter.HandleFunc("/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
+	subRouter.HandleFunc("", handlers.CreateUser).Methods("POST", "OPTIONS")
+	subRouter.HandleFunc("", handlers.GetUsers).Methods("GET", "OPTIONS")
+	subRouter.HandleFunc("/{id:[0-9]+}", handlers.GetUser).Methods("GET", "OPTIONS")
+	subRouter.HandleFunc("/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT", "OPTIONS")
+	subRouter.HandleFunc("/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE", "OPTIONS")
 }
